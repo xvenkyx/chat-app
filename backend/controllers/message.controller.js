56300bdccbdd9eb854c26594dd5ddaeb54f3a99b
xvenkyx <venkyroxx99@gmail.com>
2024-04-03 10:56:36 +0530

    user controller done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -23,19 +23,37 @@ export const sendMessage = async (req, res) => {
       message,
     });
 
-    if(newMessage){
-        conversation.messages.push(newMessage._id)
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
     }
 
     await conversation.save();
     await newMessage.save();
     // res.status(201).json({message : "Message sent successfully"})
-    res.status(201).json(newMessage)
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller :", error.message);
     res.status(500).json({ error: error.message });
   }
 };
 
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
 
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); //not the reference but the messages itself
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller:", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
 //user two id = 660ba002d68de002f69ac594
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 import connect2mongo from "./db/connect2mongo.js";
 import cookieParser from "cookie-parser";
 
@@ -18,8 +19,11 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(cookieParser())
+
+//routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users",userRoutes)
 
 app.listen(PORT, () => {
   connect2mongo();
